e30a413aef78527ab57c00ba855a311cfad78448
Nick Graffis <nicholasgraffis@gmail.com>
2021-05-17 13:40:06 -0700

    updated events meta data

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -4,34 +4,37 @@ import { useRouter } from 'vue-router'
 import { formatDate } from '../../logic'
 const lang = ref('en')
 const router = useRouter()
-const routes = router.getRoutes().filter(route => route.meta.frontmatter?.date)
-  .sort((a, b) => +new Date(b.meta.frontmatter.date) - +new Date(a.meta.frontmatter.date))
-const posts = computed(() =>
-  routes.filter(i => !i.path.endsWith('.html') && i.meta.frontmatter?.lang === lang.value),
+const routes = router.getRoutes().filter((route: any) => route.meta.frontmatter?.date)
+  .sort((a: any, b: any) => +new Date(b.meta.frontmatter.date) - +new Date(a.meta.frontmatter.date))
+const posts: any = computed(() =>
+  routes.filter((i: any) => !i.path.endsWith('.html') && i.meta.frontmatter?.lang === lang.value),
 )
 </script>
 
 <template>
-  <ul class="text-left w-full">
+  <ul class="text-left w-full space-y-6 mt-2">
     <router-link
       v-for="route in posts"
       :key="route.path"
-      class="item block font-normal mb-6 mt-2 no-underline"
+      class="item block"
       :to="route.path"
     >
       <li class="no-underline flex space-x-8">
-        <div class="">
-          <Icon class="text-3xl opacity-50" :icon="route.meta.frontmatter.icon || 'carbon:unknown'" />
-        </div>
-        <div>
-          <div class="title text-lg">
+        <Icon class="text-3xl opacity-75" :icon="route.meta.frontmatter.icon || 'carbon:unknown'" />
+        <div class="group">
+          <div class="text-lg">
             {{ route.meta.frontmatter.title }}
           </div>
-          <div class="">
+          <div class="mb-2 italic">
             {{ route.meta.frontmatter.description }}
           </div>
-          <div class="time opacity-50 text-sm -mt-1">
-            {{ formatDate(route.meta.frontmatter.date) }} <span v-if="route.meta.frontmatter.duration">· {{ route.meta.frontmatter.duration }}</span>
+          <div
+            class="text-gray-500 dark:text-gray-300 transition-all duration-200 dark:group-hover:text-dracula group-hover:text-draculaDark text-sm"
+          >
+            {{ formatDate(route.meta.frontmatter.date) }}
+            <span v-if="route.meta.frontmatter.duration">·
+              {{ route.meta.frontmatter.duration }}
+            </span>
           </div>
         </div>
       </li>
--- src/pages/blog/personalsite.md
+++ src/pages/blog/personalsite.md
@@ -1,15 +1,33 @@
 ---
-title: NickGraffis.Space
+title: My new space
 date: 2021-05-15T16:00:00Z
 lang: en
 duration: 5min
 description: Created this fun static site with Vite, Vue, and some other fun stuff.
 icon: carbon-rocket
 ---
-
-# NickGraffis.Space
-
 Hey! My goal in creating my personal website was to provide a great, real world excersise for me to learn about some awesome technologies, and to provide a canvas for me to showcase a little about me and my thoughts.
 
-## So for the first part, the techside.
-This site is built with Vite, Vue, and Vite SSG. It's deployed on Netlify.
+## First, the results
+
+I really took care to make sure that I was creating something that left no stone unturned. I wanted a fast site, that did what it set out to do, well. We can take a look category by category to see why each score is so high, and what tech is behind it. _And who helped me, because there is a lot of that_.
+
+## Performance
+
+## Accessibility
+
+## Best Practices
+
+## SEO
+
+## Maintainability
+When building this, I knew I wanted to make sure that it was something I could add too quickly. If I had to spend an hour pushing up a new blog post, or minuets waiting for builds and reloads to change some css, it wouldn't get done. And what would be the point of the site then, if it never gets updated?
+
+## File based routing
+
+## Markdown rendering
+
+## The hang ups
+Obviously, this isn't perfect. And I'm open to any suggestions you may have! Here are a few known issues that I have found:
+
+## Additonal features
